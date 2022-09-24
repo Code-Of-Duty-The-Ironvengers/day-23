2730608b081c530a771f80b82953986be19f56b8
André de Albuquerque <itstheandre@gmail.com>
2022-09-24 10:34:48 +0200

    time to get some contact sport exercise.

---
+++ forms/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+
+const codeOfDuty = [
+  { name: "Asem", from: "Syria" },
+  { name: "Pelayo", from: "ESSPAIN" },
+  { name: "Tony", from: "SwagVille" },
+  { name: "Maxime", from: "Lies About Her Age DAM" },
+  { name: "Vincent", from: "Berlin" },
+  { name: "Kira", from: "Mountains" },
+  { name: "Guichermo", from: "ESSPAIN" },
+  { name: "Daiane", from: "Braziu" },
+  { name: "Tiffany", from: "Taiwan" },
+  { name: "Nuno", from: "Puerto" },
+  { name: "Nico", from: "Parijs" },
+  { name: "Diana", from: "Parijs" },
+  { name: "Joao", from: "Need For Speed Peniche" },
+  { name: "Palash", from: "UAE" },
+  { name: "Elvan", from: "Gluten Tag" },
+  { name: "Abhijeet", from: "Parijs" },
+  { name: "Soumia", from: "Morocco" },
+  { name: "Aderemi", from: "Berlin" },
+  { name: "Nana", from: "7 languages spokenville" },
+  {
+    name: "Jackson",
+    from: "Drawing smilies instead of answering FilipeStrasse",
+  },
+  {
+    name: "Dani",
+    from: "ESSPAIN",
+  },
+  {
+    name: "Filipe",
+    from: "I bless the rains down in AAAAFRICAAAA",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <input />
+      <div key="a key to the kingdom" />
+    </div>
+  );
+}
+
+export default App;
